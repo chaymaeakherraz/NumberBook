e88d6ee054f5bd361038e9ddbe2c2663269b51a1
chaymaeakherraz <chaimaeakherraz@gmail.com>
2026-05-20 03:20:25 +0100

    Initial commit

---
+++ app/src/main/java/ma/fst/numberbook/ApiResponse.java
@@ -0,0 +1,14 @@
+package ma.fst.numberbook;
+
+public class ApiResponse {
+    private boolean success;
+    private String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
